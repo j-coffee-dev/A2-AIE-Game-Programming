47183668cf1a78df29debbaa1610fc2bcfa2c044
james-m-coffee <s260139@students.aie.edu.au>
2026-02-25 16:02:03 +1100

    Q6.3: Added search_for_targ function

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <string>
 using namespace std;
 
 int linear_search(int* array, int size, int target) {
@@ -16,6 +17,7 @@ int linear_search(int* array, int size, int target) {
     /// @return  if target first occurs as element array[i] return index i,
     ///          else return -1 if target doesn't equal any element.
     /////////////////////////////////////////////////
+
     for (int i = 0; i < size; i++) {
         if (array[i] == target) {
             return i;
@@ -23,15 +25,47 @@ int linear_search(int* array, int size, int target) {
     }
     return -1;
 }
+void search_for_targ(int* array, int size) {
+    /////////////////////////////////////////////////
+    /// @brief  Loops through user input and searches the array 
+    ///         for the input using linear_search.
+    ///         Print to let user know if that element was found.
+    ///
+    /// @param  array  : A pointer to the first element of an array of type int.
+    /// @param  size   : The number of elements in array.
+    /// 
+    /// @return  Nothing.
+    /////////////////////////////////////////////////
+
+    string target;
+    int target_int;
+    cout << "Continue entering values to search the array for (type Q to quit): " << endl;
+
+    while(true){
+        cin >> target;
+        try {
+            target_int = stoi(target);
+        }
+        catch (...) {
+            break;
+        }
+        int result = linear_search(array, size, target_int);
+        if (result == -1) { cout << "Result not found." << endl; }
+        else { cout << "Result found at index " << result << endl; }
+    }
+}
 
 int main()
 {
     //Question 6 - Linear Search
+    cout << "---------------------------Question 6---------------------------" << endl;
     const int num_of_elements = 20;
     int arr[num_of_elements] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
     assert(linear_search(arr, num_of_elements, 3) == 2);
     assert(linear_search(arr, num_of_elements, 7) == 11);
     assert(linear_search(arr, num_of_elements, 67) == 0);
     assert(linear_search(arr, num_of_elements, 88) == -1);
-    cout << "ASSERTS PASSED: LINEAR SEARCH ELEMENTS ARE IN CORRECT LOCATIONS." << endl;
+    cout << endl << "linear_search ASSERTS PASSED" << endl;
+
+    search_for_targ(arr, num_of_elements);
 }
